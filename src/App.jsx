2f78734d077ba9be90ca647da54bff09fee88d03
shejoycn <shejoycn@gmail.com>
2023-11-23 14:42:24 +0400

    first

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
+
+import TabButton from "./components/TabButton/TabButton";
+import {CORE_CONCEPTS} from './data'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2> Core Concenpts</h2>
+          <ul>
+
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+            
+          </ul>
+        </section>
+        <section id="examples">
+        <h2>Examples</h2>
+        <menu>
+        <TabButton>Components</TabButton>
+        </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TabButton/TabButton.jsx
@@ -0,0 +1,5 @@
+export default function TabButton(props) {
+    return (
+        <l1><button>{props.children}</button></l1>
+    );
+}
